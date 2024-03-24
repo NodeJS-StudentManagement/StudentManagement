7b9843f5e748187d9c9aaa22cfd43c24c4984e4c
Berkan Serbes <berkan_serbes_11@hotmail.com>
2024-03-24 16:49:35 +0300

    Student controller tamamlandı

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const studentRoute = require("./routes/studentRoute");
+const departmentRoute = require("./routes/departmentRoute");
+
+app.use(express.json()); // req.body'yi kullanabilmek için middleware tanımladık
+
+app.use("/api/v1/students", studentRoute);
+app.use("/api/v1/departments", departmentRoute);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
