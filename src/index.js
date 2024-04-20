082da09c7aa4f153d950bfddf9957c390f3f2f62
Berkan Serbes <berkan_serbes_11@hotmail.com>
2024-04-20 17:45:57 +0300

    email and counter route added to index.js file

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ const { createTables } = require("./config/db");
 
 const studentRoute = require("./routes/studentRoute");
 const departmentRoute = require("./routes/departmentRoute");
+const emailRoute = require("./routes/emailRoute");
+const studentCounterRoute = require("./routes/studentCounterRoute");
 
 app.use(express.json()); // req.body'yi kullanabilmek için middleware tanımladık
 
 app.use("/api/v1/students", studentRoute);
 app.use("/api/v1/departments", departmentRoute);
+app.use("/api/v1/email", emailRoute);
+app.use("/api/v1/counter", studentCounterRoute);
 
 const PORT = process.env.PORT || 5000;
 
@@ -22,7 +26,7 @@ const init = async () => {
   }
 };
 
-//init();
+init();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
