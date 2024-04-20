b1ac36820ae3c79dca127a1eecb3f5367568b30e
Berkan Serbes <berkan_serbes_11@hotmail.com>
2024-04-20 23:33:25 +0300

    feat: send email with scheduler

--- src/config/email.js
+++ src/config/email.js
@@ -1,42 +1,19 @@
 require("dotenv").config();
-const { writeFile, writeFileSync } = require("fs");
+const fs = require("fs");
 const { pool } = require("./db");
 const nodemailer = require("nodemailer");
 const schedule = require("node-schedule");
 
-const EMAIL_PERIOD = process.env.EMAIL_PERIOD;
-
-const transporter = nodemailer.createTransport({
-  service: "outlook",
-  secure: true,
-  port: 465,
-  auth: {
-    user: process.env.EMAIL_FROM,
-    pass: process.env.EMAIL_FROM_PASSWORD,
-  },
-});
-
-const message = {
-  from: process.env.EMAIL_FROM,
-  to: process.env.EMAIL_TO,
-  subject: "Haftalık Öğrenci Listesi",
-  text: "Haftalık öğrenci listesi ekte bulunmaktadır",
-  attachments: [
-    {
-      filename: "students.json",
-      path: "students.json",
-    },
-  ],
-};
+var json;
 
 const createStudentListJSONFile = async () => {
   const query = "SELECT * FROM students";
   const filePath = "students.json";
   try {
     const { rows: students } = await pool.query(query); // Öğrenci verilerini çekiyoruz
-    const json = JSON.stringify(students, null, 1); // JSON formatına çeviriyoruz. İlk parametre veri, ikinci parametre boşluk sayısı, üçüncü parametre ise satır atlamaları belirtir
+    json = JSON.stringify(students, null, 1); // JSON formatına çeviriyoruz. İlk parametre veri, ikinci parametre boşluk sayısı, üçüncü parametre ise satır atlamaları belirtir
 
-    writeFile(filePath, json, { encoding: "utf-8" }, (err) => {
+    fs.writeFile(filePath, json, { encoding: "utf-8" }, (err) => {
       if (err) {
         console.log(err);
       }
@@ -48,14 +25,50 @@ const createStudentListJSONFile = async () => {
   }
 };
 
+const transporter = nodemailer.createTransport({
+  service: "outlook",
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_FROM_PASSWORD,
+  },
+});
+
 const sendMail = async () => {
+  const filePath = "./students.json";
   try {
     await createStudentListJSONFile();
-    await transporter.sendMail(message);
+
+    const messageConfig = {
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: "Haftalık Öğrenci Listesi",
+      text: `Haftalık öğrenci listesi ekte bulunmaktadır veya aşağıdan kolayca erişebilirsiniz. \n \n Students: \n ${json}`,
+      attachments: [
+        {
+          path: filePath,
+          filename: "students.json",
+        },
+      ],
+    };
+
+    await transporter.sendMail(messageConfig);
+    console.log("Mail gönderildi");
   } catch (err) {
     console.log(err);
     throw err;
   }
 };
 
-module.exports = { sendMail };
+const scheduleSendMail = () => {
+  schedule.scheduleJob(process.env.EMAIL_PERIOD || "", async () => {
+    // process.env.EMAIL_PERIOD değeri .env dosyasında belirtilen süre aralığında mail gönderme işlemi gerçekleşir.
+    // Şu andaki veriye göre her çarşamba saat 9'da mail gönderme işlemi gerçekleşir
+    try {
+      await sendMail();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
+
+module.exports = { sendMail, scheduleSendMail };
